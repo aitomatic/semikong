81d076459fc8bce82e53a01ed7f72d479e9612ac
An Phan <phanhongan@gmail.com>
2024-10-25 23:04:26 -0700

    style: add empty lines

--- inference_llm_openai.py
+++ inference_llm_openai.py
@@ -1,6 +1,7 @@
-import os
-import openai
 import argparse
+import os
+
+import openai
 
 parser = argparse.ArgumentParser(description="Inference for LLM OpenAI")
 parser.add_argument('--api_key', type=str, help='Input your OpenAI API key')
@@ -9,6 +10,7 @@ api_key = args.api_key
 
 api_key = os.getenv("OPENAI_API_KEY") if api_key is None else api_key
 client = openai.OpenAI(api_key=api_key)
+
 
 def get_answer(prompt, model="gpt-4o", system_message="You are a helpful assistant."):
     response = client.chat.completions.create(
@@ -20,7 +22,7 @@ def get_answer(prompt, model="gpt-4o", system_message="You are a helpful assista
     )
     return response.choices[0].message.content
 
+
 if __name__ == "__main__":
     prompt = "What is the capital of France?"
     print(get_answer(prompt))
-    
